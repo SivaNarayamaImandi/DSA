24b14f25939f70ab18f821e577f30f20c36fa9b5
SivaNarayana <e.siva306@gmail.com>
2025-03-17 08:16:39 +0530

    Time complexity

---
+++ src/Searching/LinerSearch.java
@@ -0,0 +1,16 @@
+package Searching;
+
+public class LinerSearch {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int target = 10;
+        int index = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                index = i;
+                break;
+            }
+        }
+        System.out.println("Index of " + target + " is " + index);
+    }
+}
